b17d049c7ed81a0d4c1d7149460673dfa497a7f6
Wyatt Cowley <wycowley@gmail.com>
2021-11-02 09:46:56 -0700

    updating slideshow

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -1,102 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Motion, spring, presets } from "react-motion";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+const array = ["./images/wyattPic.jpg", "./images/familyHonduras.jpg", "./images/wildflowers.jpg"];
 
+const Slideshow = (props) => {
+    const [firstY, setFirstY] = useState(0);
+    const [secondY, setSecondY] = useState(0);
+    const [thirdY, setThirdY] = useState(0);
 
-export default class Slideshow extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {stage:0, change1:false, change2:false, change3:false, initial:0}
-    }
-    render(){
-        var array = ["./images/wyattPic.jpg","./images/familyHonduras.jpg","./images/wildflowers.jpg"]
+    useEffect(() => {
+        setFirstY(100);
+        let thing = setInterval(switchThings, 2000);
+        // return clearInterval(thing);
+    }, []);
+    function switchThings() {
+        console.log("CONSOLE");
+        if (firstY == 100) {
+            console.log("Second");
 
-        return(
-            <Motion style={{
-                raiseValue1: spring(this.state.change1 ? 0:100, {stiffness:15, damping:7.5}),
-                raiseValue2: spring(this.state.change2 ? 0:100, {stiffness:15, damping:7.5}),
-                raiseValue3: spring(this.state.change3 ? 0:100, {stiffness:15, damping:7.5})
+            setFirstY(0);
+            setSecondY(100);
+            setThirdY(0);
+            return;
+        } else if (secondY == 100) {
+            console.log("Third");
 
-            }}>
-                {interpolatedStyles =>
-                <div id="full-height full-width">
-                    <img  src={array[(2)%3]} rel="preload" style=
-                    {{height: `${interpolatedStyles.raiseValue1}%`, zIndex: `${(this.state.stage+3)%3*this.state.initial}`}}
-                    className = {" full-width full-height openerSlide "}
-                    />
-                    <img  src={array[(3)%3]} rel="preload" style=
-                        {{height: `${interpolatedStyles.raiseValue2}%`, zIndex: `${(this.state.stage+1)%3}`}}
-                        className = {" full-width full-height openerSlide "}
-                    />
-                    <img  src={array[(1)%3]} rel="preload" style=
-                        {{height: `${interpolatedStyles.raiseValue3}%`, zIndex: `${(this.state.stage+2)%3*this.state.initial}`}}
-                        className = {" full-width full-height openerSlide "}
-                    />
-                    <div id = {"titleContainer"}>
-                        <h1 id = {"nameTitle"}>Wyatt Cowley <a id={"lowerArrow"} href={"#aboutMe"}><img src="./images/chevron.png" className = {"arrowThing"}></img></a></h1>
-                    </div>
-                </div>
-                }
-                
-            </Motion>
+            setFirstY(0);
+            setSecondY(0);
+            setThirdY(100);
+            return;
+        } else {
+            console.log("First");
 
-        )
-    }
-    switch(){
-        if((this.state.stage)%3==0){
-            this.setState({
-                stage: (this.state.stage+1),
-                change3: false,
-                change2: true,
-                initial:1
-            })
+            setFirstY(100);
+            setSecondY(0);
+            setThirdY(0);
+            return;
         }
-        else if((this.state.stage+1)%3==0){
-            this.setState({
-                stage: (this.state.stage+1),
-                change1: false,
-                change3: true,
-                initial:1
-
-            })
-        }
-        else{
-            this.setState({
-                stage: (this.state.stage+1),
-                change2: false,
-                change1: true,
-                initial:1
-            })
-        }
-        console.log("initial "+this.state.initial)
-
-        
-        // this.setState({
-        //     change: !this.state.change
-        // })
     }
-    componentDidMount() {
-        console.log("initial "+this.state.initial)
-        setTimeout(()=>{
-            console.log("initial "+this.state.initial)
+    return (
+        <div id='full-height full-width'>
+            <motion.img src={array[0]} rel='preload' className={" full-width full-height openerSlide "} animate={{ height: firstY + "%" }} />
+            <motion.img src={array[1]} rel='preload' className={" full-width full-height openerSlide "} animate={{ height: secondY + "%" }} />
+            <motion.img src={array[2]} rel='preload' className={" full-width full-height openerSlide "} animate={{ height: thirdY + "%" }} />
+            <div id={"titleContainer"}>
+                <h1 id={"nameTitle"}>
+                    Wyatt Cowley{" "}
+                    <a id={"lowerArrow"} href={"#aboutMe"}>
+                        <img src='./images/chevron.png' className={"arrowThing"}></img>
+                    </a>
+                </h1>
+            </div>
+        </div>
+    );
+};
+export default Slideshow;
 
-            this.intervalClearer = setInterval(() => this.switch(),6000)
-
-        }, 4000)
-    }
-    // called when destroyed
-    componentWillUnmount() {
-        clearInterval(this.intervalClearer)
-    }
-}
-class FancyImg extends React.Component {
-    constructor(props){
-        super(props)
-    }
-
-    render(){
-        return(
-            <img className = {" full-width full-height openerSlide "} src={this.props.src}></img>
-        )
-    }
-}
+const FancyImg = (props) => {
+    return <img className={" full-width full-height openerSlide "} src={props.src}></img>;
+};
