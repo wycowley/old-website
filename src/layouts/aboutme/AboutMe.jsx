3fec120fbbad07a3962d403dd0b70dd7dd8d9b8f
Wyatt Cowley <wycowley@gmail.com>
2022-04-16 14:42:48 -0700

    updates!

--- src/layouts/aboutme/AboutMe.jsx
+++ src/layouts/aboutme/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = (props) => {
     return (
         <div>
             <BrowserView>
-                <motion.div className='about-section-container' animate={{ opacity: [0, 1] }}>
+                <div className='about-section-container'>
                     <div className='about-slideshow-container'>
                         <Slideshow array={["family.webp", "halfzion.webp", "star.webp"]} overlap={0}></Slideshow>
                     </div>
@@ -19,8 +19,8 @@ const AboutMe = (props) => {
                             In my family, I have two siblings, Cyrus and Tessa, my dog, Kody, and my two parents. After graduating, I plan on attending college, and I'm in the application process right now!
                         </p>
                     </div>
-                </motion.div>
-                <motion.div className='about-section-container'>
+                </div>
+                <div className='about-section-container'>
                     <div className='about-text-container'>
                         <h1>Work Experience</h1>
                         <p>
@@ -28,8 +28,12 @@ const AboutMe = (props) => {
                             <a href='https://www.peeryacademy.com/' className='link' target='_blank' rel='noopener'>
                                 Peery Piano Academy
                             </a>{" "}
-                            as one of their Lead Guides, teaching piano to 5+ students. I have played piano for over ten years and have performed pieces such as Sonate Pathetique and Aufschwung. <br />
-                            <br /> This year, I've become a Head Lifeguard at Mountain View Aquatics, leading shifts and trainings.
+                            as one of their Lead Guides, teaching piano to 5+ students. I have played piano for over ten years and have performed pieces such as Sonate Pathetique and Aufschwung. Check out my portfolio{" "}
+                            <a href='https://wyattcowley.com/music/' className='link' target='_blank' rel='noopener'>
+                                here
+                            </a>
+                            !<br />
+                            <br /> I lifeguard and teach swim lessons at my local pool. This year, I've started to lead shifts and open/close the pool.
                             <br />
                             <br /> I also participate and lead a couple of clubs at my high school. I am the President of the Practical Skills Club, and am part of the CS and Business clubs.
                         </p>
@@ -37,25 +41,26 @@ const AboutMe = (props) => {
                     <div className='about-slideshow-container'>
                         <Slideshow array={["piano2.webp", "chess.webp", "wakeboard.webp"]} overlap={0}></Slideshow>
                     </div>
-                </motion.div>
-                <motion.div className='about-section-container'>
+                </div>
+                <div className='about-section-container'>
                     <div className='about-slideshow-container'>
                         <Slideshow array={["eagle.webp", "zionind.webp", "shasta.webp"]} overlap={0}></Slideshow>
                     </div>
                     <div className='about-text-container'>
                         <h1>Interests</h1>
                         <p>
-                            At school I play Water Polo and Swimming, and have played for all 4 years, on varsity my Junior and Senior years. <br />
+                            At school I play Water Polo and Swimming, and have played for all 4 years, on varsity my Junior and Senior years. I am Varsity Swim Captain this year.
+                            <br />
                             <br />I am a member of the Church of Jesus Christ of Latter Day Saints, and attend weekly devotionals and activities with my church and youth congregation. I am also a Eagle Scout, receiving my award in Sophomore year. <br />
                             <br />
                             In my free time, I love to read epic fantasy, spend time in the outdoors, and play Minecraft bedwars with my friends. I love going on adventures with my communities: such as scaling Mt. Shasta or scuba diving in Honduras.
                         </p>
                     </div>
-                </motion.div>
+                </div>
             </BrowserView>
             {/* ONLY RENDERED FOR MOBILE */}
             <MobileView>
-                <motion.div className='about-section-container no-flex' animate={{ opacity: [0, 1] }}>
+                <div className='about-section-container no-flex'>
                     <div className='about-text-container full-width'>
                         <h1>Welcome</h1>
                         <p>
@@ -67,8 +72,8 @@ const AboutMe = (props) => {
                     <div className='about-slideshow-container full-width-slideshow'>
                         <Slideshow array={["family.webp", "halfzion.webp", "star.webp"]} overlap={0}></Slideshow>
                     </div>
-                </motion.div>
-                <motion.div className='about-section-container no-flex'>
+                </div>
+                <div className='about-section-container no-flex'>
                     <div className='about-text-container full-width'>
                         <h1>Work Experience</h1>
                         <p>
@@ -76,8 +81,12 @@ const AboutMe = (props) => {
                             <a href='https://www.peeryacademy.com/' className='link' target='_blank'>
                                 Peery Piano Academy
                             </a>{" "}
-                            as one of their Lead Guides, teaching piano to 5+ students. I have played piano for over ten years and have performed pieces such as Sonate Pathetique and Aufschwung. <br />
-                            <br /> This year, I've become a Head Lifeguard at Mountain View Aquatics, leading shifts and trainings.
+                            as one of their Lead Guides, teaching piano to 5+ students. I have played piano for over ten years and have performed pieces such as Sonate Pathetique and Aufschwung. Check out my portfolio{" "}
+                            <a href='https://wyattcowley.com/music/' className='link' target='_blank' rel='noopener'>
+                                here
+                            </a>
+                            !<br />
+                            <br /> I lifeguard and teach swim lessons at my local pool. This year, I've started to lead shifts and open/close the pool.
                             <br />
                             <br /> I also participate and lead a couple of clubs at my high school. I am the President of the Practical Skills Club, and am part of the CS and Business clubs.
                         </p>
@@ -85,12 +94,13 @@ const AboutMe = (props) => {
                     <div className='about-slideshow-container full-width-slideshow'>
                         <Slideshow array={["piano2.webp", "chess.webp", "wakeboard.webp"]} overlap={0}></Slideshow>
                     </div>
-                </motion.div>
-                <motion.div className='about-section-container no-flex'>
+                </div>
+                <div className='about-section-container no-flex'>
                     <div className='about-text-container full-width'>
                         <h1>Interests</h1>
                         <p>
-                            At school I play Water Polo and Swimming, and have played for all 4 years, on varsity my Junior and Senior years. <br />
+                            At school I play Water Polo and Swimming, and have played for all 4 years, on varsity my Junior and Senior years. I am Varsity Swim Captain this year.
+                            <br />
                             <br />I am a member of the Church of Jesus Christ of Latter Day Saints, and attend weekly devotionals and activities with my church and youth congregation. I am also a Eagle Scout, receiving my award in Sophomore year. <br />
                             <br />
                             In my free time, I love to read epic fantasy, spend time in the outdoors, and play Minecraft bedwars with my friends. I love going on adventures with my communities: such as scaling Mt. Shasta or scuba diving in Honduras.
@@ -99,7 +109,7 @@ const AboutMe = (props) => {
                     <div className='about-slideshow-container full-width-slideshow'>
                         <Slideshow array={["eagle.webp", "zionind.webp", "shasta.webp"]} overlap={0}></Slideshow>
                     </div>
-                </motion.div>
+                </div>
             </MobileView>
         </div>
     );
--- src/layouts/projects/Projects.jsx
+++ src/layouts/projects/Projects.jsx
@@ -5,17 +5,18 @@ import "./Projects.css";
 const Projects = (props) => {
     const projectData = [];
     projectData.push({
+        name: "Music Portfolio",
+        description: "I created this portfolio to showcase my piano progression over the years.  I used Vue and the Youtube iFrame API to build it.",
+        src: "piano.svg",
+        link: "https://www.wyattcowley.com/music/",
+    });
+    projectData.push({
         name: "Template Manager",
         description: "Create, Edit, Monitor, and Share code templates - all at the touch of a button!  Created for MVHacks 4.5 and won 3rd best overall hack.",
         src: "folder.webp",
         link: "https://www.wyattcowley.com/template-manager/#/dashboard",
     });
-    projectData.push({
-        name: "Chess Clock",
-        description: "Created to learn vue.js, this chess clock was designed to be functional cross platform and aesthetically pleasing.",
-        src: "clock.webp",
-        link: "https://www.wyattcowley.com/chess-clock-vue/",
-    });
+
     projectData.push({
         name: "Phrase Repository",
         description: "Created for the Supabase Hackathon with a friend.  Designed to be a place to share pickup lines, roasts, or compliments.  Not currently functional.",
@@ -29,23 +30,12 @@ const Projects = (props) => {
         src: "sudoku.webp",
         link: "https://github.com/wycowley/SudokuSolver",
     });
-    projectData.push({
-        name: "Songs without Words",
-        description: "Songs without Words Op.67 No.2, composed by Mendelssohn.  Performed and recorded for a virtual recital.",
-        src: "piano.svg",
-        link: "https://youtu.be/3WeVTxZj_ss",
-    });
-    projectData.push({
-        name: "Sonate Pathetique",
-        description: "Sonate Pathetique Mvmt. 1.  Written by Beethovan, this is one of the most recognizable Beethovan Sonatas.  Performed live at a recital.",
-        src: "piano.svg",
-        link: "https://youtu.be/zltBrEg72Ng",
-    });
+
     projectData.push({
         name: "Resume",
-        description: "My Resume, where you can find some more of my accomplishments.",
+        description: "My potentially outdated Resume, where you can find some more of my accomplishments.",
         src: "resume.webp",
-        link: "./projects/resume.pdf",
+        link: "resume.pdf",
     });
 
     return (
@@ -59,6 +49,7 @@ const Projects = (props) => {
                     return <IndProject name={data.name} description={data.description} src={data.src} link={data.link}></IndProject>;
                 })}
             </div>
+            <p style={{ textAlign: "center" }}>More to come soon!</p>
         </div>
     );
 };
